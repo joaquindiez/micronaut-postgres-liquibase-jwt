c080fb49c41eff46202705275f029254ef141fa2
Joaquin Diez <joaquindiez@gmail.com>
2024-05-30 11:45:18 +0200

    fix problem with OpenAPI and Kotlin, missing kapt

--- src/main/kotlin/com/example/controllers/BrandsV2Controller.kt
+++ src/main/kotlin/com/example/controllers/BrandsV2Controller.kt
@@ -1,17 +1,15 @@
 package com.example.controllers
 
-/*
-import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.responses.ApiResponse
-import io.swagger.v3.oas.annotations.responses.ApiResponses
-import io.swagger.v3.oas.annotations.tags.Tag
-*/
 import com.example.dto.BrandsDto
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.rules.SecurityRule
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import java.util.UUID
 import net.datafaker.Faker
 import org.slf4j.LoggerFactory
@@ -21,7 +19,7 @@ class BrandsV2Controller {
 
     val logger = LoggerFactory.getLogger(this::class.java)
 
-    /*   @Tag(name = "Brand-Model-Version")
+    @Tag(name = "Brand-Model-Version")
     @Operation(
             summary = "Obtener lista de Marcas ",
             description = "Recupera la lista de Marcas de Vehiculos en SwichtFleet",
@@ -34,7 +32,6 @@ class BrandsV2Controller {
             ),
             ApiResponse(responseCode = "404", description = "No encontrado")
     )
-    */
     @Get("/")
     @Secured(SecurityRule.IS_ANONYMOUS)
     fun findAllBrands(): HttpResponse<List<BrandsDto>> {
